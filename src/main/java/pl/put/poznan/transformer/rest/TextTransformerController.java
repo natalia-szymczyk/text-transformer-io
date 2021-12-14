b70cd9147275d29700d2612784459b75777961b0
Jan Świątek <jan.swiatek@student.put.poznan.pl>
2021-12-14 23:27:53 +0100

    / and /transform endpoints

--- src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -1,24 +1,29 @@
 package pl.put.poznan.transformer.rest;
 
+import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.transformer.logic.TextTransformer;
-import java.util.Arrays;
 
 @RestController
-@RequestMapping("/{text}")
 public class TextTransformerController {
     private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
 
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public String get(@PathVariable String text, @RequestParam(value = "transformations", defaultValue = "upper") String[] transformations) {
-        // Log the parameters
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
+    public String index() {
+        // TODO: List of available endpoints
+
+        return "Index";
+    }
+
+    @RequestMapping(value = "/transform/{text}", method = RequestMethod.GET, produces = "application/json")
+    public String transform(@PathVariable String text, @RequestParam(value = "transformations", defaultValue = "upper") String[] transformations) {
         logger.debug(text);
         logger.debug(Arrays.toString(transformations));
 
-        // Perform the transformation, you should run your logic here, below is just a silly example
         TextTransformer transformer = new TextTransformer(transformations);
+
         return transformer.transform(text);
     }
 }
