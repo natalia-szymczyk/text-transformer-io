260a7d807a25360e0793b109cd550f202f384a45
Mateusz Świercz <mateusz.swiercz@student.put.poznan.pl>
2021-12-16 22:04:08 +0100

    javadocs

--- src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
+++ src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
@@ -3,8 +3,14 @@ package pl.put.poznan.transformer.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * dupa javadoc
+ */
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.transformer.rest"})
 public class TextTransformerApplication {
+    /**
+     * dupa javadoc
+     */
     public static void main(String[] args) {
         SpringApplication.run(TextTransformerApplication.class, args);
     }
---
+++ start.bat
@@ -0,0 +1,13 @@
+@echo off
+set ANT_HOME=D:\Program Files\apache-ant-1.10.12
+set JAVA_HOME=C:\Users\matip\AppData\Local\Programs\AdoptOpenJDK\jdk-11.0.8.10-hotspot
+set PATH=%ANT_HOME%\bin;%PATH%
+@echo on
+@echo "You are ready to use Ant :)."
+@echo off
+set MAVEN_HOME=D:\Program Files\apache-maven-3.8.3
+set PATH=%MAVEN_HOME%\bin;%PATH%
+@echo on
+@echo "You are ready to use Maven :)."
+@echo off
+cmd /k
