eb5c1d65fe549b665d4baebc0692c698178eb342
natalia-szymczyk <nataliaszymczyk17@gmail.com>
2021-12-22 18:03:17 +0100

    big polish letters

--- src/main/java/pl/put/poznan/transformer/logic/PolishTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/PolishTransformer.java
@@ -13,26 +13,50 @@ public class PolishTransformer extends Decorator{
             if(text.charAt(i) == 'ą'){
                 result.append('a');
             }
+            else if(text.charAt(i) == 'Ą'){
+                result.append('A');
+            }
             else if(text.charAt(i) == 'ć'){
                 result.append('c');
+            }
+            else if(text.charAt(i) == 'Ć'){
+                result.append('C');
             }
             else if(text.charAt(i) == 'ę'){
                 result.append('e');
             }
+            else if(text.charAt(i) == 'Ę'){
+                result.append('E');
+            }
             else if(text.charAt(i) == 'ł'){
                 result.append('l');
+            }
+            else if(text.charAt(i) == 'Ł'){
+                result.append('L');
             }
             else if(text.charAt(i) == 'ó'){
                 result.append('o');
             }
+            else if(text.charAt(i) == 'Ó'){
+                result.append('O');
+            }
             else if(text.charAt(i) == 'ś'){
                 result.append('s');
+            }
+            else if(text.charAt(i) == 'Ś'){
+                result.append('S');
             }
             else if(text.charAt(i) == 'ż'){
                 result.append('z');
             }
+            else if(text.charAt(i) == 'Ż'){
+                result.append('Z');
+            }
             else if(text.charAt(i) == 'ź'){
                 result.append('z');
+            }
+            else if(text.charAt(i) == 'Ź'){
+                result.append('Z');
             }
             else{
                 result.append(text.charAt(i));
